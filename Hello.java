dec61515f65adac706ababf65411102d7267f465
Shruthi <shruthisasindran2006@gmail.com>
2026-06-10 10:08:05 +0530

    My first Java internship commit

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=100)
+        {
+            if(i!=101){
+                System.out.print( ",");
+
+            }
+            System.out.print(i);
+            i++;
+        }
+    }
+    
+}
